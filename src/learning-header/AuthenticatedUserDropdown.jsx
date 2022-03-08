0abf66d56c79389606fde28822f89bad3aecd5aa
Asad Iqbal <7334669+asadiqbal08@users.noreply.github.com>
2022-03-08 11:01:16 +0500

    asadiqbal08/Added user full name option (#3)
    * Added user full name option
    
    * nit

--- src/learning-header/AuthenticatedUserDropdown.jsx
+++ src/learning-header/AuthenticatedUserDropdown.jsx
@@ -24,7 +24,7 @@ const AuthenticatedUserDropdown = ({ intl, username }) => {
         <Dropdown.Toggle variant="outline-primary">
           <FontAwesomeIcon icon={faUserCircle} className="d-md-none" size="lg" />
           <span data-hj-suppress className="d-none d-md-inline">
-            {username}
+            {name}
           </span>
         </Dropdown.Toggle>
         <Dropdown.Menu className="dropdown-menu-right">
@@ -51,7 +51,7 @@ const AuthenticatedUserDropdown = ({ intl, username }) => {
 
 AuthenticatedUserDropdown.propTypes = {
   intl: intlShape.isRequired,
-  username: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default injectIntl(AuthenticatedUserDropdown);
